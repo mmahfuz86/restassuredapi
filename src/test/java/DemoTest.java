0b77d7f0af7e0a177757fa492caf78312f1ff140
Mahfuz <alhaque.mahfuz@gmail.com>
2020-09-16 20:34:50 -0400

    added testcases ReqresGetTest

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,28 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void practiceTest() {
+
+        String url = "https://reqres.in/api/users/2";
+        RestAssured
+                .given()
+                    .get(url)
+                    .andReturn()
+                    .getBody()
+                    .prettyPrint();
+    }
+
+    @Test
+    public void practiceTest2() {
+        String url = "https://swapi.dev/api/people/1/";
+        Response response = RestAssured.given()
+                .get(url)
+                .andReturn();
+        response.getBody().prettyPrint();
+
+    }
+}
